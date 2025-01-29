b5a2415d972445d4a6cc46654acad4b154baed93
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2025-01-29 15:26:46 +0900

    feat: :sparkles: new blog article on deepseek

--- src/components/PageLinks.tsx
+++ src/components/PageLinks.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import clsx from 'clsx'
+import Link from 'next/link'
 
 import { Border } from '@/components/Border'
 import { Container } from '@/components/Container'
@@ -69,8 +69,7 @@ export function PageLinks({
   className?: string
 }) {
   return (
-    <div className={clsx('relative pt-24 sm:pt-32 lg:pt-40', className)}>
-
+    <div className={clsx('relative pt-24', className)}>
       <SectionIntro title={title} smaller>
         {intro && <p>{intro}</p>}
       </SectionIntro>
